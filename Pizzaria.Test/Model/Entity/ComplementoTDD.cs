f32c6f341a43f1ee413b345139d79a2e609e1a4e
mikemajesty <mikemajesty2011@gmail.com>
2015-12-21 09:16:48 -0200

    Complement test

---
+++ Pizzaria.Test/Model/Entity/ComplementoTDD.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pizzaria.Model.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pizzaria.Test.Model.Entity
+{
+    /// <summary>
+    /// Summary description for Complemeto
+    /// </summary>
+    [TestClass]
+    public class ComplementoTDD
+    {
+      
+
+        [TestMethod]
+        public void Complemento_Test_Atribuicao()
+        {
+            var com = new Complemento
+            {
+                 Descricao = "hsdhhs hsbd hxhb   h bhsb hsb hbsh sh b",
+                  Preco = 5,
+                    SaborID = 1
+            };
+            IList<ValidationResult> erros = new List<ValidationResult>();
+            if (!Validator.TryValidateObject(com, new ValidationContext(com), erros, true))
+            {
+
+                //erro
+
+            }
+            else
+            {
+                //valido
+            }
+        }
+    }
+}
