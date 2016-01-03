6fed3917f0d608bd9567fe2861a23e9bb782d387
mikemajesty <mikemajesty2011@gmail.com>
2016-01-03 11:16:45 -0200

    Adjustments to the edge model.

--- Pizzaria.Model/Entity/Borda.cs
+++ Pizzaria.Model/Entity/Borda.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pizzaria.Model.Entity
 {
@@ -13,21 +7,16 @@ namespace Pizzaria.Model.Entity
     public class Borda
     {
         [Key]
-        [Range(type: typeof(int), minimum: "0", maximum: "60000", ErrorMessage = "{0} excedido, contate o administrador")]
-        [DisplayName(displayName: "Borda ID")]
+        [Range(type: typeof(int), minimum: "0", maximum: "60000", ErrorMessage = "Borda ID excedido, contate o administrador")]
         public int BordaID { get; set; }
-        [Required(ErrorMessage = "{0} é obrigatório.")]
-        [StringLength(maximumLength: 30, ErrorMessage = "{0} deve conter pelo menos 30 letras.")]
-        [DisplayName(displayName: "Borda Nome")]
+        [StringLength(maximumLength: 30, ErrorMessage = "Nome da Borda deve conter pelo menos 30 letras.")]
         public string Nome { get; set; }
-        [Range(type: typeof(double), minimum: "0", maximum: "50", ErrorMessage = "{0} incorreto.")]
-        [DisplayName("Preço de venda")]
-        [DataType(DataType.Currency, ErrorMessage = "{0} invalido.")]
-        public double Preco { get; set; }
-        [StringLength(maximumLength: 50, ErrorMessage = "{0} deve conter pelo menos 50 letras.")]
-        [DisplayName(displayName: "Borda Descrição")]
+        [Range(type: typeof(decimal), minimum: "0,01", maximum: "50", ErrorMessage = "Preço de venda deve ser maior que 0.0 R$ e menor que 50.00 R$.")]
+        [DataType(DataType.Currency, ErrorMessage = "Preço de venda inválido.")]
+        public decimal Preco { get; set; }
+        [StringLength(maximumLength: 50, ErrorMessage = "Descrição da borda deve conter no máximo 50 letras.")]
         public string Descricao { get; set; }
-     
+
 
     }
 }
