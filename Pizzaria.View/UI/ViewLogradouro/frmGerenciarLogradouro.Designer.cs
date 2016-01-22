b0f0474c90784fd0813f59b66fd8b7abad2ba3fb
mikemajesty <mikemajesty2011@gmail.com>
2016-01-22 18:16:06 -0200

    Adicionar form de Gerenciar Logradouro

---
+++ Pizzaria.View/UI/ViewLogradouro/frmGerenciarLogradouro.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Pizzaria.View.UI.ViewLogradouro
+{
+    partial class frmGerenciarLogradouro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGerenciarLogradouro));
+            this.gpbGerenciarLogradouro = new System.Windows.Forms.GroupBox();
+            this.gpbPesquisar = new System.Windows.Forms.GroupBox();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.dgvLogradouro = new System.Windows.Forms.DataGridView();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.gpbGerenciarLogradouro.SuspendLayout();
+            this.gpbPesquisar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLogradouro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gpbGerenciarLogradouro
+            // 
+            this.gpbGerenciarLogradouro.Controls.Add(this.gpbPesquisar);
+            this.gpbGerenciarLogradouro.Controls.Add(this.btnSair);
+            this.gpbGerenciarLogradouro.Controls.Add(this.btnDeletar);
+            this.gpbGerenciarLogradouro.Controls.Add(this.btnEditar);
+            this.gpbGerenciarLogradouro.Controls.Add(this.btnNovo);
+            this.gpbGerenciarLogradouro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbGerenciarLogradouro.Location = new System.Drawing.Point(12, 9);
+            this.gpbGerenciarLogradouro.Name = "gpbGerenciarLogradouro";
+            this.gpbGerenciarLogradouro.Size = new System.Drawing.Size(425, 277);
+            this.gpbGerenciarLogradouro.TabIndex = 2;
+            this.gpbGerenciarLogradouro.TabStop = false;
+            this.gpbGerenciarLogradouro.Text = "Gerenciar Cidade";
+            // 
+            // gpbPesquisar
+            // 
+            this.gpbPesquisar.Controls.Add(this.txtPesquisa);
+            this.gpbPesquisar.Controls.Add(this.dgvLogradouro);
+            this.gpbPesquisar.Location = new System.Drawing.Point(6, 78);
+            this.gpbPesquisar.Name = "gpbPesquisar";
+            this.gpbPesquisar.Size = new System.Drawing.Size(411, 190);
+            this.gpbPesquisar.TabIndex = 5;
+            this.gpbPesquisar.TabStop = false;
+            this.gpbPesquisar.Text = "Pesquisar Logradouro";
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtPesquisa.Location = new System.Drawing.Point(24, 26);
+            this.txtPesquisa.MaxLength = 50;
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(370, 26);
+            this.txtPesquisa.TabIndex = 1;
+            this.txtPesquisa.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dgvLogradouro
+            // 
+            this.dgvLogradouro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLogradouro.Location = new System.Drawing.Point(0, 65);
+            this.dgvLogradouro.Name = "dgvLogradouro";
+            this.dgvLogradouro.Size = new System.Drawing.Size(411, 125);
+            this.dgvLogradouro.TabIndex = 0;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Image = ((System.Drawing.Image)(resources.GetObject("btnSair.Image")));
+            this.btnSair.Location = new System.Drawing.Point(337, 25);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(80, 47);
+            this.btnSair.TabIndex = 1;
+            this.btnSair.TabStop = false;
+            this.btnSair.Text = "Sair";
+            this.btnSair.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Image = ((System.Drawing.Image)(resources.GetObject("btnDeletar.Image")));
+            this.btnDeletar.Location = new System.Drawing.Point(218, 25);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(102, 47);
+            this.btnDeletar.TabIndex = 2;
+            this.btnDeletar.TabStop = false;
+            this.btnDeletar.Text = "Deletar";
+            this.btnDeletar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Image = ((System.Drawing.Image)(resources.GetObject("btnEditar.Image")));
+            this.btnEditar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEditar.Location = new System.Drawing.Point(107, 25);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(92, 47);
+            this.btnEditar.TabIndex = 3;
+            this.btnEditar.TabStop = false;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Image = ((System.Drawing.Image)(resources.GetObject("btnNovo.Image")));
+            this.btnNovo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNovo.Location = new System.Drawing.Point(6, 25);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(85, 47);
+            this.btnNovo.TabIndex = 4;
+            this.btnNovo.TabStop = false;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // frmGerenciarLogradouro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(454, 296);
+            this.Controls.Add(this.gpbGerenciarLogradouro);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmGerenciarLogradouro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logradouro";
+            this.gpbGerenciarLogradouro.ResumeLayout(false);
+            this.gpbPesquisar.ResumeLayout(false);
+            this.gpbPesquisar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLogradouro)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpbGerenciarLogradouro;
+        private System.Windows.Forms.GroupBox gpbPesquisar;
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.DataGridView dgvLogradouro;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnNovo;
+    }
+}
